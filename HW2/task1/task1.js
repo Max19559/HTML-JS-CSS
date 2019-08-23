886d2862c3099066a99c83133c739dc4d2d3871a
Max19559 <maksim.yukho@leverx.com>
2019-08-23 05:10:43 -0700

    Done HW2

---
+++ HW2/task1/task1.js
@@ -0,0 +1,31 @@
+function showNewPage (){
+    location.href = "https://www.google.com";
+}
+
+function deleteAndShowText (){
+    var text = document.getElementById("textBlock");
+    if(text.style.display === "none"){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
+
+function getName(){
+
+    var name = prompt("Enter your name","Ivan");
+    var check = parseInt(name.replace( /\D/g, '')) || 0;
+    var newName="";
+    if(!check){
+        for(var i=0; i<name.length;i++){
+            if(name[i].toUpperCase()===name[i]){
+                newName += name[i].toLowerCase();
+            } else{
+                newName += name[i].toUpperCase();
+            }
+        }
+        alert(newName);
+    } else{
+        alert(name.split("").reverse().join(""));
+    }
+}
